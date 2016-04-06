30ca390e9dbb55b6a3367ab2de8e041c6af918b7
jareda <jareda@localhost>
2016-04-06 16:48:03 -0400

    Finished Test Two application.

---
+++ TestDogBLC/src/edu/saintjoe/cs/brianc/testdogblc/TestDogClass.java
@@ -0,0 +1,24 @@
+package edu.saintjoe.cs.brianc.testdogblc;
+
+public class TestDogClass {
+
+	public static void main(String[] args) {
+	}
+	TestDogBLC bark = new TestDogBLC(); 
+		// TODO Auto-generated method stub
+		private int height;
+		private String name;
+	public void bark() 
+{
+			    
+		    	
+		    	if (height > 60 ) {
+		          System.out.println("Woof!! Woof!! says " + name + " thats height is " + height);
+		    	} else if (height > 14 ) {
+		    		System.out.println("Ruff!! Ruff!! says " + name + " thats height is " + height);
+		    	} else
+		    		System.out.println("Yip!! Yip!! says " + name + " thats height is " + height);
+		    
+	}
+
+}
